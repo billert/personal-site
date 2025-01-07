8f4a933fe9c2c124942802dcac30e0e78c5a4395
Owen Cai <owenbcai@gmail.com>
2025-01-07 01:30:57 -0600

    smooth navbar selection transitions

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
     return <header className = "z-10 sticky pl-1 pr-1 top-0 flex flex-row items-center justify-between align-middle h-12 w-full bg-[repeating-linear-gradient(45deg,var(--tw-gradient-stops))] from-[#efefef] dark:from-[#090909] from-[length:0_20px] to-[#f5f5f5] dark:to-[#161616] to-[length:20px_40px]">
         <div className = "flex flex-row gap-1">
 
-            <Link href = "/" className = "flex flex-row items-center align-middle p-1 hover:bg-[#00000010] dark:hover:bg-[#ffffff10] active:bg-[#00000060] dark:active:bg-[#ffffff60]">
+            <Link href = "/" className = "transition-all flex flex-row items-center align-middle p-1 hover:bg-[#00000010] dark:hover:bg-[#ffffff10] active:bg-[#00000060] dark:active:bg-[#ffffff60]">
                 <div className = "transition ease-in-out flex flex-row">
                     <p className = "text-3xl mr-0 font-bold">billert</p>
                     <p className = "text-3xl ml-0 font-bold text-transparent text-stroke-black dark:text-stroke-white text-stroke-1">.dev</p>
@@ -22,7 +22,7 @@ export default function Navbar() {
             {pageList.map((item) => {
                 return <li key={item.key} className = "list-none flex flex-row align-middle items-center justify-center"><Link href = {"/"+item.name} className= "flex flex-row align-middle items-center justify-center">
                 <div className = "transition ease-in-out flex flex-row">
-                    <p className = {pathname.includes("/"+item.name) ? "opacity-100  bg-[#00000020] dark:bg-[#ffffff30] p-2" : "opacity-70 p-2 hover:bg-[#00000010] dark:hover:bg-[#ffffff10] active:bg-[#00000060] dark:active:bg-[#ffffff60]"}>{item.displayName}</p>
+                    <p className = {pathname.includes("/"+item.name) ? "opacity-100  bg-[#00000020] dark:bg-[#ffffff30] p-2" : "transition-colors opacity-70 p-2 hover:bg-[#00000010] dark:hover:bg-[#ffffff10] active:bg-[#00000060] dark:active:bg-[#ffffff60]"}>{item.displayName}</p>
                 </div>
             </Link> </li>
             })}
