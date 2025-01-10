5c5876697a116cee27ad6df0c032f2b909259b14
Owen Cai <owenbcai@gmail.com>
2025-01-10 15:59:05 -0600

    navbar blur

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -11,40 +11,53 @@ const socialList = [{key: 0, url: "https://codeforces.com/profile/billert12", lo
 export default function Navbar() {
     const pathname = usePathname();
     const [showMenu, setShowMenu] = useState(false);
-    return <header className = "z-10 sticky pl-1 pr-1 top-0 flex flex-row items-center justify-between align-middle h-12 w-full bg-[repeating-linear-gradient(45deg,var(--tw-gradient-stops))] from-[#efefef] dark:from-[#090909] from-[length:0_20px] to-[#f5f5f5] dark:to-[#161616] to-[length:20px_40px]">
-        <div className = "flex flex-row gap-1">
+    return <header className = "z-10 sticky top-0">
+        <div className = "pl-1 pr-1 flex flex-row items-center justify-between align-middle h-12 w-full bg-[repeating-linear-gradient(45deg,var(--tw-gradient-stops))] from-[#efefef] dark:from-[#090909] from-[length:0_20px] to-[#f5f5f5] dark:to-[#161616] to-[length:20px_40px]">
+            <div className = "flex flex-row gap-1">
 
-            <Link href = "/" className = "transition-all flex flex-row items-center align-middle p-1 hover:bg-[#00000010] dark:hover:bg-[#ffffff10] active:bg-[#00000060] dark:active:bg-[#ffffff60]">
-                <div className = "transition ease-in-out flex flex-row">
-                    <p className = "text-3xl mr-0 font-bold">billert</p>
-                    <p className = "text-3xl ml-0 font-bold text-transparent text-stroke-black dark:text-stroke-white text-stroke-1">.dev</p>
+                <Link href = "/" className = "transition-all flex flex-row items-center align-middle p-1 hover:bg-[#00000010] dark:hover:bg-[#ffffff10] active:bg-[#00000060] dark:active:bg-[#ffffff60]">
+                    <div className = "transition ease-in-out flex flex-row">
+                        <p className = "text-3xl mr-0 font-bold">billert</p>
+                        <p className = "text-3xl ml-0 font-bold text-transparent text-stroke-black dark:text-stroke-white text-stroke-1">.dev</p>
+                    
+                    </div>
+                </Link>
+                {pageList.map((item) => {
+                    return <li key={item.key} className = "list-none hidden md:flex flex-row align-middle items-center justify-center"><Link href = {"/"+item.name} className= "flex flex-row align-middle items-center justify-center">
+                    <div className = "transition ease-in-out flex flex-row">
+                        <p className = {pathname.includes("/"+item.name) ? "opacity-100  bg-[#00000020] dark:bg-[#ffffff30] p-2" : "transition-colors opacity-70 p-2 hover:bg-[#00000010] dark:hover:bg-[#ffffff10] active:bg-[#00000060] dark:active:bg-[#ffffff60]"}>{item.displayName}</p>
+                    </div>
+                </Link> </li>
+                })}
+                {/*<p className = "">Path: {pathname}</p>*/}
                 
-                </div>
-            </Link>
-            {pageList.map((item) => {
-                return <li key={item.key} className = "list-none hidden lg:flex flex-row align-middle items-center justify-center"><Link href = {"/"+item.name} className= "flex flex-row align-middle items-center justify-center">
-                <div className = "transition ease-in-out flex flex-row">
-                    <p className = {pathname.includes("/"+item.name) ? "opacity-100  bg-[#00000020] dark:bg-[#ffffff30] p-2" : "transition-colors opacity-70 p-2 hover:bg-[#00000010] dark:hover:bg-[#ffffff10] active:bg-[#00000060] dark:active:bg-[#ffffff60]"}>{item.displayName}</p>
-                </div>
-            </Link> </li>
-            })}
-            {/*<p className = "">Path: {pathname}</p>*/}
-            
-        </div>
-        <div className = "flex flew-row gap-1">
-            {socialList.map((item) => {
-                return <li key = {item.key} className = "list-none hidden md:flex">
-                    <a href = {item.url}>
-                        <Image aria-hidden src={item.logo} alt={item.name} width={32} height={32} className="dark:invert">
+            </div>
+            <div className = "flex flew-row gap-1">
+                {socialList.map((item) => {
+                    return <li key = {item.key} className = "list-none hidden md:flex">
+                        <a href = {item.url}>
+                            <Image aria-hidden src={item.logo} alt={item.name} width={32} height={32} className="dark:invert">
 
-                        </Image>
-                    </a>
-                </li>
-            })}
-            <a href = "#" className = "flex flex-row items-center align-middle md:hidden" onClick = {() => {setShowMenu(!showMenu)}}>
-                <p>Menu?</p>
-                {showMenu && <p> Still work in progress...</p>}
-            </a>
+                            </Image>
+                        </a>
+                    </li>
+                })} {/*       V this makes the link do nothing  */}
+                <Link href = "javascript:;" className = "flex flex-row items-center align-middle md:hidden gap-1" onClick = {() => {setShowMenu(!showMenu); return false;}}>
+                    <p>Menu</p>
+                    
+                </Link>
+            </div>
+        </div>
+        <div className = {`${showMenu ? "" : "hidden h-0"} bg-[#dddddda0] dark:bg-[#222222a0] backdrop-blur-md transition-all`}>
+            <div className = "flex flex-col">
+                {pageList.map((item) => {
+                    return <li key={item.key} className = "list-none md:hidden flex-row align-middle items-center justify-center"><Link href = {"/"+item.name} className= "flex flex-row align-middle items-center justify-center">
+                    <div className = "transition ease-in-out flex flex-row">
+                        <p className = {pathname.includes("/"+item.name) ? "opacity-100  bg-[#00000020] dark:bg-[#ffffff30] p-2" : "transition-colors opacity-70 p-2 hover:bg-[#00000010] dark:hover:bg-[#ffffff10] active:bg-[#00000060] dark:active:bg-[#ffffff60]"}>{item.displayName}</p>
+                    </div>
+                </Link> </li>
+                })}
+            </div>
         </div>
     </header>
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="z-0 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <p className = "mb-20 mt-20 text-5xl font-bold">The personal website of Owen Cai</p>
+        <p className = "mb-20 mt-20 text-5xl font-bold">The personal website of ??????? (lol)</p>
         <a href = "https://billert.carrd.co/">[go to carrd website]</a>
         <p className = "mb-20 mt-20 text-5xl">very long text to test scrolling</p>
         <p className = "mb-20 mt-20 text-5xl">very long text to test scrolling</p>
