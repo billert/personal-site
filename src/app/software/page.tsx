0a8999b7efc1a2bba6cede3e63f5eb10b19b7cb3
Owen Cai <owenbcai@gmail.com>
2025-01-10 23:31:20 -0600

    basic functionality for software page project carousel

--- src/app/software/page.tsx
+++ src/app/software/page.tsx
@@ -1,14 +1,36 @@
-//import Image from "next/image";
+"use client"
+
+import React from 'react'
+import Image from "next/image"
+import useEmblaCarousel from 'embla-carousel-react'
+
+
+
+const projects = [{key: 0, name: "Maroon Menu", desc: "A menu checker for Texas A&M Dining Halls that just works", images: ["/maroon menu 1.png", "/maroon menu 2.png", "/maroon menu 3.png", "/maroon menu 4.png", "/maroon menu 5.png"]}]
 
 export default function Page() {
+const [emblaRef] = useEmblaCarousel({dragFree: true})
   return (
     <div className = "">
-    <div className="z-0 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="z-0 grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        
-        <p className = "mb-20 mt-20 text-5xl">you are on the software page</p>
-        <p className = "mb-20 mt-20 text-5xl">check back soon lol</p>
+        <div>
+            <div className = "bg-[#dddddd] dark:bg-[#222222] flex flex-col justify-start w-[600] p-2">
+                <p className = "font-bold text-6xl">Maroon Menu</p>
+                <p className = "leading-5 w-[350] text-lg mb-2">A menu checker for Texas A&M Dining Halls that just works</p>
+                <div className="embla overflow-hidden" ref={emblaRef}>
+                    <div className="embla__container flex gap-2 flex-shrink-0">
+                        {projects[0].images.map((item, index) => {
+                            return <div key = {index} className = "embla__slide flex-shrink-0">
+                                <Image src = {item} alt = "Preview screenshot" height = {300} width = {150} className = "rounded-3xl"></Image>
+                            </div>
+                        })}
+                    </div>
+                </div>
+                
+            </div>
+        </div>
         
       </main>
       
