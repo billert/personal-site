a3bcd2f2459cbe9a4dc7f749e5a7ebff77596750
Owen Cai <owenbcai@gmail.com>
2025-05-11 04:01:41 -0500

    fancy animations for front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,10 +18,23 @@ export default function Page() {
     <div className="z-0 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        
-        <p className = "text-5xl">Hi, I&apos;m <span className = "font-bold">Owen Cai</span>,</p>
-        <p className = "text-2xl opacity-70">Student at Texas A&M</p>
-        <motion.a layout href="./owen cai resume m8.pdf" download="Owen Cai Resume.pdf" className = "p-4 flex flex-row justify-between items-center gap-2 border-2 border-black dark:border-white" onMouseEnter={() => set_arrow_state("hover")} onMouseLeave={() => set_arrow_state("normal")} onMouseDown = {() => set_arrow_state("prime")} onMouseUp = {() => {set_arrow_state("activate"); setResumeButton("Thank you!")}}>
+        <div>
+          <p className = "text-5xl"><motion.span style={{opacity:0}} animate={{opacity:1}} transition={{duration:0.4, delay:0}}>Hi,</motion.span><motion.span style={{opacity:0}} animate={{opacity:1}} transition={{duration:0.4, delay:0.4}}> I&apos;m </motion.span><motion.span className = "font-bold" style={{opacity:0}} animate={{opacity:1}} transition={{duration:0.4, delay:0.6}}>Owen Cai</motion.span><motion.span style={{opacity:0}} animate={{opacity:1}} transition={{duration:0.4, delay:0.8}}>,</motion.span></p>
+
+          <motion.p className = "text-2xl" style={{opacity:0}} animate={{opacity:0.7}} transition={{delay:1.2}}>Student at Texas A&M.</motion.p>
+
+        </div>
+        <div>
+          <motion.p className = "text-2xl mb-0" style={{opacity:0}} animate={{opacity:0.7}} transition={{delay:2.5}}>I&apos;m also a...</motion.p>
+          <motion.ul className = "m-0" >
+          {combined_list.map((item, key) => {
+                      return <motion.li key={key} className = "list-none m-0 p-0 gap-0" style={{opacity:0, x:-10}} animate={{opacity:1,x:0}} transition={{delay:3+key*0.6, duration:0.5}}>
+                        <p><span className = "font-bold">{item.what}</span> at {item.who}</p>
+                      </motion.li>
+                  })}
+          </motion.ul>
+        </div>
+        <motion.a layout href="./owen cai resume m8.pdf" download="Owen Cai Resume.pdf" style={{opacity:0, y:5}} animate={{opacity:1, y:0}} transition={{delay: 4+combined_list.length*0.6, duration:1}} className = "p-4 flex flex-row justify-between items-center gap-2 border-2 border-black dark:border-white" onMouseEnter={() => set_arrow_state("hover")} onMouseLeave={() => set_arrow_state("normal")} onMouseDown = {() => set_arrow_state("prime")} onMouseUp = {() => {set_arrow_state("activate"); setResumeButton("Thank you!")}}>
           <p className = "text-3xl">{resumeButton}</p>
           <motion.img src="/arrow-right.svg" alt="Right arrow of Go" style={{rotate:90}} height={45} width={45} animate={arrow_state} variants={{
             normal: {y: 0, opacity:0.7},
@@ -30,14 +43,6 @@ export default function Page() {
             hover: {y: 0, opacity: 0.9}
           }} className="dark:invert"></motion.img>
         </motion.a>
-        <p className = "text-2xl opacity-70 mb-0">I&apos;m also a...</p>
-        <ul className = "m-0">
-        {combined_list.map((item, key) => {
-                    return <li key={key} className = "list-none m-0 p-0 gap-0">
-                      <p><span className = "font-bold">{item.what}</span> at {item.who}</p>
-                    </li>
-                })}
-        </ul>
       </main>
     </div>
     </div>
