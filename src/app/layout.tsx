d90e2fec503de2b6f2c128370fcbe48281aa228b
Owen Cai <owenbcai@gmail.com>
2025-05-12 01:46:52 -0500

    install vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ViewTransitions } from 'next-view-transitions'
+import { Analytics } from "@vercel/analytics/next"
 
 import Navbar from "./navbar";
 
@@ -35,7 +36,7 @@ export default function RootLayout({
           
             <Navbar/>
             {children}
-          
+            <Analytics />
         </body>
       </html>
     </ViewTransitions>
